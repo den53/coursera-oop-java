c6d0d840ede6dffc7f8ebea090fa9a45425adf30
Dennis A. Seidel <mail@dennisseidel.de>
2015-10-03 19:44:27 +0200

    Add background image

--- src/courseexamples/module2/MyPApplet.java
+++ src/courseexamples/module2/MyPApplet.java
@@ -1,6 +1,18 @@
 package courseexamples.module2;
 
-public class MyPApplet {
+import processing.core.*;
+
+public class MyPApplet extends PApplet {
+	private String URL = "https://www.unbelievable-machine.com/wp-content/uploads/2015/05/i40_2-bitkom.jpg";
+	private PImage backgroundImg;
 	
+	public void setup() {
+		size(200, 200);
+		backgroundImg = loadImage(URL, "jpg");
+	}
+	public void draw() {
+		backgroundImg.resize(0, height);
+		image(backgroundImg, 0, 0);
+	}
 
 }
